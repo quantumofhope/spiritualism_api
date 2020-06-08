b0b4df2fb61b281971aacfe52ef8822dc2cfa49e
quantumofhope <62652455+quantumofhope@users.noreply.github.com>
2020-06-08 23:01:07 +0100

    Create web.py

---
+++ web.py
@@ -0,0 +1,12 @@
+import flask from Flask, jsonify
+app=Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return "Hello World!!"
+  
+@app.route('/api')
+def api():
+  return jsonify({"status":"ok", "payload":("name":"John", "place":"Bournemouth"})
+  
